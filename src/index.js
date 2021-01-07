d8f8ae7d07dc1591750415521df4f1367d7f089f
Josh Carter <joshcarter8400@gmail.com>
2021-01-07 16:58:00 -0500

    added components setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="ui-container comments">
+      <CommentDetail
+        pic={faker.image.avatar()}
+        author="Sam"
+        timeAgo="Today at 4:45"
+        blogComment="WOW!!"
+      />
+      <CommentDetail
+        pic={faker.image.avatar()}
+        author="Alex"
+        timeAgo="Today at 5:45"
+        blogComment="That's it?!"
+      />
+      <CommentDetail
+        pic={faker.image.avatar()}
+        author="Jane"
+        timeAgo="Yesterday at 4:45"
+        blogComment="Nice Work!!"
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
