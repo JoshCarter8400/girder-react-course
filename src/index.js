47225d4873a4f92491d18fd572c5951d9e8545be
Josh Carter <joshcarter8400@gmail.com>
2021-01-11 16:53:57 -0500

    added class based components

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
-import CommentDetail from "./CommentDetail";
 
-const App = () => {
-  return (
-    <div className="ui-container comments">
-      <CommentDetail
-        pic={faker.image.avatar()}
-        author="Sam"
-        timeAgo="Today at 4:45"
-        blogComment="WOW!!"
-      />
-      <CommentDetail
-        pic={faker.image.avatar()}
-        author="Alex"
-        timeAgo="Today at 5:45"
-        blogComment="That's it?!"
-      />
-      <CommentDetail
-        pic={faker.image.avatar()}
-        author="Jane"
-        timeAgo="Yesterday at 4:45"
-        blogComment="Nice Work!!"
-      />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude:{this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
